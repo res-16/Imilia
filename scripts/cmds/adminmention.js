4968de0b6ac1bb9b6fa7111e4fed39f3e4d8c634
res-16 <zres16@gmail.com>
2026-05-17 19:55:55 +0100

    Update adminmention.js

--- scripts/cmds/adminmention.js
+++ scripts/cmds/adminmention.js
@@ -13,7 +13,8 @@ module.exports = {
   onStart: async function () {},
 
   onChat: async function ({ event, message }) {
-    const adminIDs = ["100078049308655", "100090071683807", "100092480994957"].map(String);
+    const adminIDs = ["100056125717291", "100056125717291", "100056125717291
+                      "].map(String);
 
     // Skip if sender is admin
     if (adminIDs.includes(String(event.senderID))) return;
